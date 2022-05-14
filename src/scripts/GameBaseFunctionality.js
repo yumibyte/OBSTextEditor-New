fce06d438e03b29e362731a0ef3f8d3448cd4d7b
Yumibyte <ashraigosa@gmail.com>
2022-05-14 11:54:45 -0700

    fix navigation between pages

--- src/scripts/GameBaseFunctionality.js
+++ src/scripts/GameBaseFunctionality.js
@@ -1,6 +1,8 @@
 
-const stopBtn = document.getElementById('stopBtn');
 
+// const gamePanelOW = document.getElementById('gamePanelOW');
+const gamePanelOW = document.querySelector('div').id;
+console.log(gamePanelOW);
 gamePanelOW.onclick = e => {
 
   console.log("Hello World!");
