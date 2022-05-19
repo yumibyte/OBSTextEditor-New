a8b4a08030190213c9365362306c24ad3ec0eadf
Yumibyte <ashraigosa@gmail.com>
2022-05-19 12:13:53 -0700

    changed to onclick functions

--- src/scripts/GameBaseFunctionality.js
+++ src/scripts/GameBaseFunctionality.js
@@ -1,10 +1,8 @@
 
+function button() {
+  console.log("test");
+}
 
-// const gamePanelOW = document.getElementById('gamePanelOW');
-const gamePanelOW = document.querySelector('div').id;
-console.log(gamePanelOW);
-gamePanelOW.onclick = e => {
-
-  console.log("Hello World!");
-
-};
+function myFunction(){
+  console.log("hi");
+}
