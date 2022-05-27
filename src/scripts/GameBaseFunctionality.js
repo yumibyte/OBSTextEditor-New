8926ed15f817fb24a6857ce5862c4f78c14fd610
Yumibyte <ashraigosa@gmail.com>
2022-05-27 11:05:49 -0700

    load object into local storage

--- src/scripts/GameBaseFunctionality.js
+++ src/scripts/GameBaseFunctionality.js
@@ -72,51 +72,30 @@ function generateTitle() {
 
 function readCSV() {
 
+  // read file
   var fileUpload = document.getElementById("filePathInput");
   var filePath = "../" + fileUpload.value;
   console.log(filePath);
   d3.tsv(filePath, function (d) {
-    console.log(d);
+
+    for (var i = 0; i < d.length; i++) {
+      d[i]["Display"] = "test";
+      delete d['columns'];
+    }
+    // Put the object into storage
+    localStorage.setItem('currentRoster', JSON.stringify(d));
   });
+
 }
 
-        // var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.tsv|.txt)$/;
-        // if (regex.test(fileUpload.value.toLowerCase())) {
-        //     if (typeof (FileReader) != "undefined") {
-        //         var reader = new FileReader();
-        //         reader.onload = function (e) {
-        //             var table = document.createElement("table");
-        //             var rows = e.target.result.split("\n");
-        //             for (var i = 0; i < rows.length; i++) {
-        //                 var cells = rows[i].split("");
-        //                 if (cells.length > 1) {
-        //                     var row = table.insertRow(-1);
-        //                     for (var j = 0; j < cells.length; j++) {
-        //                         var cell = row.insertCell(-1);
-        //                         console.log(cells[j]);
-        //                     }
-        //                 }
-        //             }
-        //             // var dvCSV = document.getElementById("dvCSV");
-        //             // dvCSV.innerHTML = "";
-        //             // dvCSV.appendChild(table);
-        //         }
-        //         reader.readAsText(fileUpload.files[0]);
-        //     } else {
-        //         alert("This browser does not support HTML5.");
-        //     }
-        // } else {
-        //     alert("Please upload a valid CSV file.");
-        // }
 
 
 function updatePlayerNameDropdown() {
   var playerNameDropdown = document.getElementById("playerNameDropdown");
-  var playerRoster = [
-    ["Matthew Chance Lopez", "type", 1],
-    ["Mason Harder", "type1", 1],
-    ["Jordan Hopper", "type2", 1]
-  ]
+
+  // Retrieve the object from storage
+  var retrievedObject = localStorage.getItem('currentRoster');
+  console.log(JSON.parse(retrievedObject));
 
   for (i in playerRoster) {
 
