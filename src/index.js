58d9af9619a407d74f333e71782ae775f8c080a1
Yumibyte <ashraigosa@gmail.com>
2022-06-01 02:11:59 -0700

    generated stream alert and code cleanup

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-// const fs = require('fs');
-
-// function writeTextFile() {
-//   fs.writeFile('TextText.txt', localStorage.getItem("title"), (err) => {
-//       if (err) throw err;
-//   })
-// }
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -40,31 +32,6 @@ const createWindow = () => {
 }
 
 
-
-
-
-
-// const createWindow = () => {
-//   // Create the browser window.
-//   const mainWindow = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     webPreferences: {
-//       nodeIntegration: true,
-//       enableRemoteModule: true,
-//       contextIsolation: false,
-//       // preload: path.join(__dirname, 'preload.js')
-//
-//     }
-//   });
-//
-//   // and load the index.html of the app.
-//   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-//
-//   // Open the DevTools.
-//   mainWindow.webContents.openDevTools();
-// };
-
 app.whenReady().then(() => {
   createWindow()
 
@@ -78,34 +45,6 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
-
-
-
-
-
-//
-// // This method will be called when Electron has finished
-// // initialization and is ready to create browser windows.
-// // Some APIs can only be used after this event occurs.
-// app.on('ready', createWindow);
-//
-// // Quit when all windows are closed.
-// app.on('window-all-closed', () => {
-//   // On OS X it is common for applications and their menu bar
-//   // to stay active until the user quits explicitly with Cmd + Q
-//   if (process.platform !== 'darwin') {
-//     app.quit();
-//   }
-// });
-//
-// app.on('activate', () => {
-//   // On OS X it's common to re-create a window in the app when the
-//   // dock icon is clicked and there are no other windows open.
-//   if (BrowserWindow.getAllWindows().length === 0) {
-//     createWindow();
-//   }
-// });
-
 
 
 // In this file you can include the rest of your app's specific main process
--- src/scripts/GameBaseFunctionality.js
+++ src/scripts/GameBaseFunctionality.js
@@ -250,6 +250,8 @@ function generateStream() {
     }
   }
 
+  alert("Generated stream!");
+
 }
 
 
